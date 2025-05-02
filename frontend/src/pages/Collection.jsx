681fb77f522d65fa480c55e3caa1cb387e9c4edb
abhilashtk0922 <abhilashtk2004@gmail.com>
2025-05-02 20:02:40 +0530

    Initial commit

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -139,7 +139,7 @@ const Collection = () => {
         <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 gap-y-6 p-20'>
           {
             filterProducts.map((item,index)=>(
-              <ProductItem key={index} name={item.name} id={item._id} price={item.price} image={item.image} />
+              <ProductItem key={index} name={item.name} id={item._id}  image={item.image} />
             ))
           }
         </div>
