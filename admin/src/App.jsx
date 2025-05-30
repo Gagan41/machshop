3a7f20e45f22ad4124aba4e6816257cf3eeb0a39
Gagan41 <gagant.p100@gmail.com>
2025-05-30 13:03:41 +0530

    video v0.0.6 final

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,6 +5,7 @@ import { Routes, Route } from "react-router-dom";
 import Add from "./pages/Add";
 import List from "./pages/List";
 import Orders from "./pages/Orders";
+import Videos from "./pages/Videos";
 import Login from "./components/Login";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -12,36 +13,38 @@ import "react-toastify/dist/ReactToastify.css";
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 const App = () => {
+  const [token, setToken] = useState(
+    localStorage.getItem("token") ? localStorage.getItem("token") : ""
+  );
 
-  const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):'');
-
-  useEffect(()=>{
-    localStorage.setItem('token',token)
-  },[token])
+  useEffect(() => {
+    localStorage.setItem("token", token);
+  }, [token]);
 
   return (
-    <div className='bg-gray-50 min-h-screen'>
+    <div className="bg-gray-50 min-h-screen">
       <ToastContainer />
-      {token === ""
-        ? <Login setToken={setToken} />
-        : <>
+      {token === "" ? (
+        <Login setToken={setToken} />
+      ) : (
+        <>
           <Navbar setToken={setToken} />
           <hr />
-          <div className='flex w-full'>
+          <div className="flex w-full">
             <Sidebar />
-            <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+            <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
               <Routes>
-                <Route path='/add' element={<Add token={token} />} />
-                <Route path='/list' element={<List token={token} />} />
-                <Route path='/orders' element={<Orders token={token} />} />
+                <Route path="/add" element={<Add token={token} />} />
+                <Route path="/list" element={<List token={token} />} />
+                <Route path="/orders" element={<Orders token={token} />} />
+                <Route path="/videos" element={<Videos token={token} />} />
               </Routes>
             </div>
           </div>
         </>
-      }
+      )}
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
